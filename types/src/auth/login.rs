a9e2a3472b622546aa8ce076e1dd4554cb26ebef
MedzikUser <nivua1fn@duck.com>
2022-04-19 23:12:48 +0200

    feat: zeroize the user's unencrypted password, and other unencrypted values from memory

--- types/src/auth/login.rs
+++ types/src/auth/login.rs
@@ -1,12 +1,14 @@
 use serde::{Deserialize, Serialize};
+use zeroize::{Zeroize, ZeroizeOnDrop};
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone, Zeroize, ZeroizeOnDrop)]
 pub struct Request {
+    #[zeroize(skip)]
     pub username: String,
     pub password: String,
 }
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+#[derive(Debug, Serialize, Deserialize, Clone, Zeroize, ZeroizeOnDrop)]
 pub enum Response {
     LoggedIn { access_token: String },
 }
