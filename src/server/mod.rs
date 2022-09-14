2ade3291fe498e55d4d3a19f6c5c5ea69589138a
MedzikUser <medzik@duck.com>
2022-09-14 20:11:46 +0200

    feat(http): add cors hosts
    Implement cors hosts.

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -3,7 +3,8 @@ pub mod error;
 pub mod utils;
 
 use anyhow::anyhow;
-use axum::{routing::get, Extension, Router, Server};
+use axum::{http::HeaderValue, routing::get, Extension, Router, Server};
+use tower_http::cors::{AllowOrigin, CorsLayer};
 use tracing::info;
 
 use crate::{config::Config, database::Database};
@@ -14,11 +15,20 @@ pub async fn start_server(config: Config, db: Database) -> anyhow::Result<()> {
         config.http.host, config.http.port
     );
 
+    // change the type from Vec<String> to Vec<HeaderValue> so that the http server can correctly detect CORS hosts
+    let origins = config
+        .http
+        .cors
+        .iter()
+        .map(|e| e.parse().expect("Failed to parse CORS hosts"))
+        .collect::<Vec<HeaderValue>>();
+
     let host = format!("{}:{}", config.http.host, config.http.port);
 
     let app = Router::new()
         .nest("/api", api::app())
         .route("/", get(api::health))
+        .layer(CorsLayer::new().allow_origin(AllowOrigin::list(origins)))
         .layer(Extension(config))
         .layer(Extension(db));
 
